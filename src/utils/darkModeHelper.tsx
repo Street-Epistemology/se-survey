15deff5c38754a22c074b791ded1ff62adc29466
TurturOk <aningan@zoho.com>
2021-08-21 23:33:24 +0200

    Removing css file that was replaced by scss. Darker colors for table sections' headers.

--- src/utils/darkModeHelper.tsx
+++ src/utils/darkModeHelper.tsx
@@ -11,16 +11,16 @@ export function isDarkModeEnabled(): boolean {
   return darkMode === DARK_MODE.enabled;
 }
 
-export function enableDarkMode() {
+export function enableDarkMode(): void {
   document.body.classList.add(DARK_MODE.class);
   localStorage.setItem(DARK_MODE.storageKey, DARK_MODE.enabled);
 }
 
-export function disableDarkMode() {
+export function disableDarkMode(): void {
   document.body.classList.remove(DARK_MODE.class);
   localStorage.setItem(DARK_MODE.storageKey, DARK_MODE.disabled);
 }
 
-export function toggleDarkMode() {
+export function toggleDarkMode(): void {
   isDarkModeEnabled() ? disableDarkMode() : enableDarkMode();
 }
