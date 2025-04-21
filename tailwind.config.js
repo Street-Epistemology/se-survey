b040fc9c2ce4c161e45ec31d3fdd660327042f76
Ragnar <raxsade@gmail.com>
2025-04-22 02:10:59 +0300

    feat: replace react-scripts with vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   theme: {
     extend: {},
   },
